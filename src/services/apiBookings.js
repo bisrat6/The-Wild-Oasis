3dfcd9ef5166bcc82f76bc234e29e56c51d8c773
bisrat6 <bisratberiso@gmail.com>
2026-01-02 18:24:10 +0300

    pagination and booking details implemented

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,22 +1,37 @@
 import { getToday } from "../utils/helpers";
 
 import supabase from "./supabase";
+import { PAGE_SIZE } from "../utils/constants";
 
-export async function getBookings({filter,sortBy}) {
-  let query=supabase
+export async function getBookings({ filter, sortBy, page }) {
+  let query = supabase
     .from("bookings")
     .select(
-      "id,created_at,startDate,endDate,numNights,numGuests,status,totalPrice,cabins(name),guests(fullName,email)"
+      "id,created_at,startDate,endDate,numNights,numGuests,status,totalPrice,cabins(name),guests(fullName,email)",
+      { count: "exact" }
     );
 
-    if (filter!==null) query= query[filter.method || 'eq'](filter.field,filter.value)
+  //Filter
+  if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
 
-    const { data, error } = await query;
+  //Sort
+  if (sortBy)
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    });
+
+  if (page) {
+    const from = (page - 1) * PAGE_SIZE;
+    const to = from + PAGE_SIZE - 1;
+    query = query.range(from, to);
+  }
+
+  const { data, error, count } = await query;
   if (error) {
     console.log(error);
     throw new Error("Failed to fetch bookings");
   }
-  return data;
+  return { data, count };
 }
 
 export async function getBooking(id) {
